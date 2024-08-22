e806c3cb809a8e88466706564e123d1cafd991c0
Bell07734 <102612496+Bell07734@users.noreply.github.com>
2024-08-22 15:45:08 +0100

    Removed rickroll

--- conversion.js
+++ conversion.js
@@ -58,10 +58,6 @@ ENumbers = [
 
 function convert() {
   let digits = parseInt(input.value);
-  if (digits == 69) {
-    window.location.href = "https://www.youtube.com/watch?v=p7YXXieghto";
-    return;
-  }
   output.innerHTML = converters[language.value](digits);
 }
 
